bd703fd6267c7b65d7b37f73d5313b934ae5581f
Chinonso Kalu <tripletens.kc@gmail.com>
2021-07-10 00:06:48 +0100

    push the full working project

--- api/demo_web_app.py
+++ api/demo_web_app.py
@@ -105,5 +105,5 @@ def demo_web_app(gpt, config=UIConfig()):
             offset = len(gpt.output_prefix)
         return {'text': response['choices'][0]['text'][offset:]}
 
-    subprocess.Popen(["yarn", "start"])
+    subprocess.Popen(["yarn", "start"],shell=True)
     app.run()
